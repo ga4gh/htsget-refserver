087094a915cd975489129f5b7c1d12ad598cfc82
david-xliu <davxliu@gmail.com>
2019-11-11 14:29:23 -0500

    separate data endpoint into multiple functions

--- handler/data.go
+++ handler/data.go
@@ -30,7 +30,88 @@ func getData(w http.ResponseWriter, r *http.Request) {
 	blockID, _ := strconv.Atoi(r.Header.Get("block-id"))
 	numBlocks, _ := strconv.Atoi(r.Header.Get("num-blocks"))
 	class := r.Header.Get("class")
+	args := getCmdArgs(id, refName, start, end, numBlocks, class)
+	cmd := exec.Command("samtools", args...)
 
+	pipe, _ := cmd.StdoutPipe()
+	err = cmd.Start()
+	if err != nil {
+		panic(err)
+	}
+
+	tempPath := getTempPath(id, blockID)
+
+	fSam, _ := os.Create(tempPath)
+	defer fSam.Close()
+	reader := bufio.NewReader(pipe)
+
+	if len(fields) == 0 && refName != "*" {
+		io.Copy(fSam, reader)
+	} else {
+		processData(fSam, reader, refName, fields)
+	}
+	cmd.Wait()
+
+	samToBam(tempPath)
+	trimBlob(class, numBlocks, tempPath, blockID)
+
+	fclient, _ := os.Open(tempPath + "_bam")
+	defer fclient.Close()
+	io.Copy(w, fclient)
+}
+
+func processData(fSam *os.File, reader *bufio.Reader, refName string, fields []string) {
+	l, _, err := reader.ReadLine()
+	columns := make([]bool, 11)
+	for _, field := range fields {
+		columns[FIELDS[field]-1] = true
+	}
+
+	for ; err == nil; l, _, err = reader.ReadLine() {
+		if l[0] == 64 {
+			l = append(l, "\n"...)
+			fSam.Write(l)
+		} else {
+			var output []string
+			ls := strings.Split(string(l), "\t")
+			keepLine := true
+			if refName == "*" {
+				keepLine = isUnmappedUnplaced(ls)
+			}
+			if keepLine {
+				for i, col := range columns {
+					if col {
+						output = append(output, ls[i])
+					} else {
+						if i == 1 || i == 3 || i == 4 || i == 7 || i == 8 {
+							output = append(output, "0")
+						} else {
+							output = append(output, "*")
+						}
+					}
+				}
+				l = []byte(strings.Join(output, "\t") + "\n")
+				fSam.Write(l)
+			}
+		}
+	}
+}
+
+func getTempPath(id string, blockID int) string {
+	cwd, _ := os.Getwd()
+	tempPath := cwd + "/temp/" + id + "_" + strconv.Itoa(blockID)
+
+	if exists, _ := fileExists(cwd + "/temp"); !exists {
+		os.Mkdir(cwd+"/temp/", 0755)
+	} else {
+		if isDir, _ := isDir(cwd + "/temp"); !isDir {
+			os.Mkdir(cwd+"/temp/", 0755)
+		}
+	}
+	return tempPath
+}
+
+func getCmdArgs(id string, refName string, start string, end string, numBlocks int, class string) []string {
 	args := []string{"view", dataSource + filePath(id)}
 	var refRange string
 	if refName != "" && refName != "*" {
@@ -47,86 +128,13 @@ func getData(w http.ResponseWriter, r *http.Request) {
 	}
 	if class == "header" {
 		args = append(args, "-H")
+		if numBlocks == 1 {
+			args = append(args, "-b")
+		}
 	} else {
 		args = append(args, "-h")
 	}
-	cmd := exec.Command("samtools", args...)
-
-	pipe, _ := cmd.StdoutPipe()
-	err = cmd.Start()
-	if err != nil {
-		panic(err)
-	}
-
-	cwd, _ := os.Getwd()
-	tempPath := cwd + "/temp/" + id + "_" + strconv.Itoa(blockID)
-
-	if exists, _ := exists(cwd + "/temp"); !exists {
-		os.Mkdir(cwd+"/temp/", 0755)
-	} else {
-		if isDir, _ := isDir(cwd + "/temp"); !isDir {
-			os.Mkdir(cwd+"/temp/", 0755)
-		}
-	}
-
-	fSam, _ := os.Create(tempPath)
-	defer fSam.Close()
-	reader := bufio.NewReader(pipe)
-
-	if len(fields) == 0 && refName != "*" {
-		io.Copy(fSam, reader)
-	} else {
-		l, _, err := reader.ReadLine()
-		columns := make([]bool, 11)
-		for _, field := range fields {
-			columns[FIELDS[field]-1] = true
-		}
-
-		for ; err == nil; l, _, err = reader.ReadLine() {
-			if l[0] == 64 {
-				l = append(l, "\n"...)
-				fSam.Write(l)
-			} else {
-				var output []string
-				ls := strings.Split(string(l), "\t")
-				keepLine := true
-				if refName == "*" {
-					keepLine = isUnmappedUnplaced(ls)
-				}
-				if keepLine {
-					for i, col := range columns {
-						if col {
-							output = append(output, ls[i])
-						} else {
-							if i == 1 || i == 3 || i == 4 || i == 7 || i == 8 {
-								output = append(output, "0")
-							} else {
-								output = append(output, "*")
-							}
-						}
-					}
-					l = []byte(strings.Join(output, "\t") + "\n")
-					fSam.Write(l)
-				}
-			}
-		}
-	}
-	cmd.Wait()
-
-	samToBam(tempPath)
-
-	if class == "header" {
-		removeEOF(tempPath + "_bam")
-	} else if class == "body" {
-		removeHeader(tempPath + "_bam")
-		if blockID != numBlocks {
-			removeEOF(tempPath + "_bam")
-		}
-	}
-
-	fclient, _ := os.Open(tempPath + "_bam")
-	defer fclient.Close()
-	io.Copy(w, fclient)
+	return args
 }
 
 func samToBam(tempPath string) {
@@ -158,7 +166,7 @@ func removeEOF(tempPath string) error {
 }
 
 // exists returns whether the given file or directory existsi.
-func exists(path string) (bool, error) {
+func fileExists(path string) (bool, error) {
 	_, err := os.Stat(path)
 	if err == nil || !os.IsNotExist(err) {
 		return true, err
@@ -183,4 +191,15 @@ func isUnmappedUnplaced(l []string) bool {
 	unmapped := flag&1 == 1
 
 	return unmapped && l[2] == "*" && l[3] == "0"
+}
+
+func trimBlob(class string, numBlocks int, tempPath string, blockID int) {
+	if class == "header" && numBlocks > 1 {
+		removeEOF(tempPath + "_bam")
+	} else if class == "body" {
+		removeHeader(tempPath + "_bam")
+		if blockID != numBlocks {
+			removeEOF(tempPath + "_bam")
+		}
+	}
 }
