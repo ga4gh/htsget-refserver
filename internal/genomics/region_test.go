c341bf3f89cfb692f7001264afdcd9e3a3603e6c
xngln <davxliu@gmail.com>
2019-12-17 15:39:13 -0500

    use testify package for assertion tests

--- internal/genomics/region_test.go
+++ internal/genomics/region_test.go
@@ -4,13 +4,19 @@ import (
 	"testing"
 
 	. "github.com/david-xliu/htsget-refserver/internal/genomics"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestString(t *testing.T) {
 	r := &Region{Name: "chr1", Start: "0", End: "100"}
 	s := r.String()
+	assert.Equal(t, "chr1:0-100", s, "They should be equal")
 
-	if s != "chr1:0-100" {
-		t.Errorf("String was incorrect, got: %v, want: %v.", s, "chr1:0-100")
-	}
+	r = &Region{Name: "chr1", Start: "-1"}
+	s = r.String()
+	assert.Equal(t, "chr1", s, "They should be equal")
+
+	r = &Region{Name: "chr1", Start: "100", End: "-1"}
+	s = r.String()
+	assert.Equal(t, "chr1:100", s, "They should be equal")
 }
