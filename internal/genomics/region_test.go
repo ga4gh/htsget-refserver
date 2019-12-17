37dd727a83ea0111d00100e763b1738be58b256c
xngln <davxliu@gmail.com>
2019-12-17 16:45:52 -0500

    replace testify with standard testing library

--- internal/genomics/region_test.go
+++ internal/genomics/region_test.go
@@ -4,19 +4,24 @@ import (
 	"testing"
 
 	. "github.com/david-xliu/htsget-refserver/internal/genomics"
-	"github.com/stretchr/testify/assert"
 )
 
 func TestString(t *testing.T) {
 	r := &Region{Name: "chr1", Start: "0", End: "100"}
 	s := r.String()
-	assert.Equal(t, "chr1:0-100", s, "They should be equal")
+	if s != "chr1:0-100" {
+		t.Errorf("Got: %v, wanted: chr1:0-100", s)
+	}
 
 	r = &Region{Name: "chr1", Start: "-1"}
 	s = r.String()
-	assert.Equal(t, "chr1", s, "They should be equal")
+	if s != "chr1" {
+		t.Errorf("Got: %v, wanted: chr1", s)
+	}
 
 	r = &Region{Name: "chr1", Start: "100", End: "-1"}
 	s = r.String()
-	assert.Equal(t, "chr1:100", s, "They should be equal")
+	if s != "chr1:100" {
+		t.Errorf("Got: %v, wanted: chr1:100", s)
+	}
 }
