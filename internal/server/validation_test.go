8b6bfe560820155308854b4b5052e34ab745b1da
xngln <davxliu@gmail.com>
2019-12-20 17:22:30 -0500

    add test parameters

--- internal/server/validation_test.go
+++ internal/server/validation_test.go
@@ -4,7 +4,48 @@ import (
 	"testing"
 )
 
-func TestParseFormat(t *testing.T) {
+var params []map[string][]string = []map[string][]string{
+	map[string][]string{
+		"format":        []string{"Bam"},
+		"class":         []string{"Header"},
+		"referenceName": []string{"*"},
+		"start":         []string{"500"},
+		"end":           []string{"9000"},
+		"fields":        []string{"QNAME", "SEQ"},
+	},
+	map[string][]string{
+		"format":        []string{"CRAM"},
+		"class":         []string{"body"},
+		"referenceName": []string{"chr11"},
+		"start":         []string{"-100"},
+		"end":           []string{"0"},
+		"fields":        []string{"QNAME,NotARealField"},
+	},
+	map[string][]string{
+		"format":        []string{"Bam,Cram"},
+		"class":         []string{"Header,body"},
+		"referenceName": []string{"Chr1"},
+		"end":           []string{"900"},
+		"fields":        []string{"Tlen,SEQ"},
+	},
+	map[string][]string{
+		"format": []string{"Bam", "Cram"},
+		"start":  []string{"100"},
+		"end":    []string{"1000"},
+	},
+	map[string][]string{
+		"referenceName": []string{"Chr1"},
+	},
+	map[string][]string{
+		"referenceName": []string{"chr1"},
+		"start":         []string{"11000"},
+	},
+	map[string][]string{
+		"referenceName": []string{"chr1"},
+		"start":         []string{"100"},
+		"end":           []string{"10"},
+	},
+}
 
 }
 
