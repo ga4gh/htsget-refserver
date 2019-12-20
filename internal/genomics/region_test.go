09a7a51792995adc797cf1dd3ef4638ffc1994e6
xngln <davxliu@gmail.com>
2019-12-20 17:18:03 -0500

    add tests for genomics package

--- internal/genomics/region_test.go
+++ internal/genomics/region_test.go
@@ -1,27 +1,25 @@
-package genomics_test
+package genomics
 
 import (
 	"testing"
-
-	. "github.com/david-xliu/htsget-refserver/internal/genomics"
 )
 
 func TestString(t *testing.T) {
 	r := &Region{Name: "chr1", Start: "0", End: "100"}
 	s := r.String()
 	if s != "chr1:0-100" {
-		t.Errorf("Got: %v, wanted: chr1:0-100", s)
+		t.Errorf("Got: %v, expected: chr1:0-100", s)
 	}
 
 	r = &Region{Name: "chr1", Start: "-1"}
 	s = r.String()
 	if s != "chr1" {
-		t.Errorf("Got: %v, wanted: chr1", s)
+		t.Errorf("Got: %v, expected: chr1", s)
 	}
 
 	r = &Region{Name: "chr1", Start: "100", End: "-1"}
 	s = r.String()
 	if s != "chr1:100" {
-		t.Errorf("Got: %v, wanted: chr1:100", s)
+		t.Errorf("Got: %v, expected: chr1:100", s)
 	}
 }
