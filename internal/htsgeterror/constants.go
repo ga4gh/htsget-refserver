4c4b11168228872935c927dd012f5a78e038000f
Jeremy Adams <jeremy.adams@ga4gh.org>
2020-06-30 12:52:33 -0400

    docstrings and commenting for genomics and htsgeterror packages

--- internal/htsgeterror/constants.go
+++ internal/htsgeterror/constants.go
@@ -1,5 +1,8 @@
 // Package htsgeterror provides operations for writing various HTTP errors,
 // including different status codes and error response bodies
+//
+// Module constants.go contains HTTP Error constants, including status codes,
+// error names, and default messages
 package htsgeterror
 
 import (
@@ -7,32 +10,71 @@ import (
 	"strconv"
 )
 
-// HTTP status code constants by error encountered
+/* Error Status Codes: HTTP status code constants by error */
+
+// codeBadRequest (int): status code for Bad Request errors
 const codeBadRequest = http.StatusBadRequest
+
+// codeInvalidAuthentication (int): status code for AuthN errors
 const codeInvalidAuthentication = http.StatusUnauthorized
+
+// codePermissionDenied (int): status code for AuthZ errors
 const codePermissionDenied = http.StatusForbidden
+
+// codeNotFound (int): status code for Not Found errors
 const codeNotFound = http.StatusNotFound
+
+// codeInternalServerError (int): status code for unspecified server-side error
 const codeInternalServerError = http.StatusInternalServerError
 
-// canonical error names by error encountered
+/* Error Names: htsget canonical error names */
+
+// errorBadRequestUnsupportedFormat (string): error name for unsupported format
 const errorBadRequestUnsupportedFormat = "UnsupportedFormat"
+
+// errorBadRequestInvalidInput (string): error name for invalid input
 const errorBadRequestInvalidInput = "InvalidInput"
+
+// errorBadRequestInvalidRange (string): error name for invalid range
 const errorBadRequestInvalidRange = "InvalidRange"
+
+// errorInvalidAuthentication (string): error name for invalid AuthN
 const errorInvalidAuthentication = "InvalidAuthentication"
+
+// errorPermissionDenied (string): error for permission denied (invalid AuthZ)
 const errorPermissionDenied = "PermissionDenied"
+
+// errorNotFound (string): error name for not found
 const errorNotFound = "NotFound"
+
+// errorInternalServerError (string): error name for unspecified server errors
 const errorInternalServerError = "InternalServerError"
 
-// default error messages by error encountered
+/* Default Messages: default error message by error name */
+
+// dfltMsgBadRequestUnsupportedFormat (string): default unsupported format message
 const dfltMsgBadRequestUnsupportedFormat = "The requested file format is not supported by the server"
+
+// dfltMsgBadRequestInvalidInput (string): default invalid input message
 const dfltMsgBadRequestInvalidInput = "The request parameters do not adhere to the specification"
+
+// dfltMsgBadRequestInvalidRange (string): default invalid range message
 const dfltMsgBadRequestInvalidRange = "The requested range cannot be satisfied"
+
+// dfltMsgInvalidAuthentication (string): default AuthN error message
 const dfltMsgInvalidAuthentication = "Authorization provided is invalid"
+
+// dfltMsgPermissionDenied (string): default AuthZ error message
 const dfltMsgPermissionDenied = "Authorization is required to access the resource"
+
+// dfltMsgNotFound (string): default not found error message
 const dfltMsgNotFound = "The resource requested was not found"
+
+// dfltMsgInternalServerError (string): default message for unspecified errors
 const dfltMsgInternalServerError = "Internal server error"
 
-// maps error type to status code and default message
+// errorInfoMap (map[string]map[string]string) maps error name to status code
+// and default message
 var errorInfoMap = map[string]map[string]string{
 	errorBadRequestUnsupportedFormat: {
 		"code":    strconv.Itoa(codeBadRequest),
--- internal/htsgeterror/error.go
+++ internal/htsgeterror/error.go
@@ -1,5 +1,8 @@
 // Package htsgeterror provides operations for writing various HTTP errors,
 // including different status codes and error response bodies
+//
+// Module error.go contains operations for constructing htsget errors, and
+// writing them to the HTTP response
 package htsgeterror
 
 import (
@@ -9,26 +12,43 @@ import (
 	"strconv"
 )
 
-// htsgetError contains attributes to write an error as an HTTP response in Go.
-// contains the code, and a container for the response body
+// htsgetError contains attributes to write an error as an HTTP response
+//
+// Attributes
+//	Code (int): HTTP status code
+//	Htsget (errorContainer): contains error name and message
 type htsgetError struct {
 	Code   int
 	Htsget errorContainer `json:"htsget"`
 }
 
-// errorContainer contains attributes for an htsget error response body,
-// including the error name and a helper message
+// errorContainer contains attributes for an htsget error response body
+//
+// Attributes
+//	Error (string): htsget error name (according to htsget specification)
+//	Message (string): client help message, explaining why error was encountered
 type errorContainer struct {
 	Error   string `json:"error"`
 	Message string `json:"message"`
 }
 
 // Error displays htsgetError attributes as a string
+//
+// Type: htsgetError
+// Returns
+//	(string): string representation of the error (name and message)
 func (err *htsgetError) Error() string {
 	return fmt.Sprint(err.Htsget.Error + ": " + err.Htsget.Message)
 }
 
-// instantiates an htsgetError object with a specific code, name, and message
+// newHtsgetError instantiates a new htsgetError instance
+//
+// Arguments
+//	code (int): HTTP status code
+//	err (string): error name
+//	message (string): error message
+// Returns
+//	(htsgetError): error object with attributes set based on passed arguments
 func newHtsgetError(code int, err string, message string) *htsgetError {
 	htsgetError := &htsgetError{
 		Code: code,
@@ -40,7 +60,11 @@ func newHtsgetError(code int, err string, message string) *htsgetError {
 	return htsgetError
 }
 
-// writes a passed htsget error as json to the HTTP ResponseWriter
+// writeHTTPError writes an htsgetError code and body to the HTTP ResponseWriter
+//
+// Arguments
+//	writer (http.ResponseWriter): HTTP response writer, sets response code and body
+//	err (error): any error object, including an htsgetError
 func writeHTTPError(writer http.ResponseWriter, err error) {
 	if err, ok := err.(*htsgetError); ok {
 		writer.Header().Set("Content-Type", "application/json")
@@ -60,14 +84,20 @@ func writeHtsgetErrorToHTTPError(writer http.ResponseWriter, code int, err strin
 	writeHTTPError(writer, htsgetError)
 }
 
-// template function for more specific htsget error types. writes an htsget
-// error to the ResponseWriter. uses the default message in the response body
-// if no message is passed
+// htsgetErrorTemplate is a template function for more specific htsget error
+// types. writes an htsget error to the ResponseWriter. uses the default message
+// in the response body if no message is passed
+//
+// Arguments
+//	writer (http.ResponseWriter): HTTP response writer, sets response code and body
+//	errorName (string): htsget-specific error name
+//	msgPtr (*string): pointer to a message string, if nil, use the default message
 func htsgetErrorTemplate(writer http.ResponseWriter, errorName string, msgPtr *string) {
 	code, codeParseErr := strconv.Atoi(errorInfoMap[errorName]["code"])
 	if codeParseErr != nil {
-
 	}
+
+	// use the default message if the message pointer is nil
 	msg := errorInfoMap[errorName]["dfltMsg"]
 	if msgPtr != nil {
 		msg = *msgPtr
@@ -76,36 +106,64 @@ func htsgetErrorTemplate(writer http.ResponseWriter, errorName string, msgPtr *s
 }
 
 // UnsupportedFormat writes an UnsupportedFormat error to the HTTP ResponseWriter
+//
+// Arguments
+//	writer (http.ResponseWriter): HTTP response writer, sets response code and body
+// msgPtr (*string): pointer to message string, if nil, use the default message
 func UnsupportedFormat(writer http.ResponseWriter, msgPtr *string) {
 	htsgetErrorTemplate(writer, errorBadRequestUnsupportedFormat, msgPtr)
 }
 
 // InvalidInput writes an InvalidInput error to the HTTP ResponseWriter
+//
+// Arguments
+//	writer (http.ResponseWriter): HTTP response writer, sets response code and body
+// msgPtr (*string): pointer to message string, if nil, use the default message
 func InvalidInput(writer http.ResponseWriter, msgPtr *string) {
 	htsgetErrorTemplate(writer, errorBadRequestInvalidInput, msgPtr)
 }
 
 // InvalidRange writes an InvalidRange error to the HTTP ResponseWriter
+//
+// Arguments
+//	writer (http.ResponseWriter): HTTP response writer, sets response code and body
+// msgPtr (*string): pointer to message string, if nil, use the default message
 func InvalidRange(writer http.ResponseWriter, msgPtr *string) {
 	htsgetErrorTemplate(writer, errorBadRequestInvalidRange, msgPtr)
 }
 
 // InvalidAuthentication writes an InvalidAuthentication error to the HTTP ResponseWriter
+//
+// Arguments
+//	writer (http.ResponseWriter): HTTP response writer, sets response code and body
+// msgPtr (*string): pointer to message string, if nil, use the default message
 func InvalidAuthentication(writer http.ResponseWriter, msgPtr *string) {
 	htsgetErrorTemplate(writer, errorInvalidAuthentication, msgPtr)
 }
 
 // PermissionDenied writes a PermissionDenied error to the HTTP ResponseWriter
+//
+// Arguments
+//	writer (http.ResponseWriter): HTTP response writer, sets response code and body
+// msgPtr (*string): pointer to message string, if nil, use the default message
 func PermissionDenied(writer http.ResponseWriter, msgPtr *string) {
 	htsgetErrorTemplate(writer, errorPermissionDenied, msgPtr)
 }
 
 // NotFound writes a NotFound error to the HTTP ResponseWriter
+//
+// Arguments
+//	writer (http.ResponseWriter): HTTP response writer, sets response code and body
+// msgPtr (*string): pointer to message string, if nil, use the default message
 func NotFound(writer http.ResponseWriter, msgPtr *string) {
 	htsgetErrorTemplate(writer, errorNotFound, msgPtr)
 }
 
 // InternalServerError writes an InternalServerError error to the HTTP ResponseWriter
+//
+// Arguments
+//	writer (http.ResponseWriter): HTTP response writer, sets response code and body
+// msgPtr (*string): pointer to message string, if nil, use the default message
 func InternalServerError(writer http.ResponseWriter, msgPtr *string) {
 	htsgetErrorTemplate(writer, errorInternalServerError, msgPtr)
 }
