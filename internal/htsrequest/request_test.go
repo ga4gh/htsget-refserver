b439463692d56398cbd6eae99bdfa526da515e0c
Jeremy Adams <jeremy.adams@ga4gh.org>
2020-08-26 08:27:58 -0400

    fixed test for construct data url from htsrequest object

--- internal/htsrequest/request_test.go
+++ internal/htsrequest/request_test.go
@@ -1,25 +1,55 @@
 package htsrequest
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/ga4gh/htsget-refserver/internal/htsconstants"
 	"github.com/stretchr/testify/assert"
 )
 
+var requestConstructDataEndpointURLTC = []struct {
+	endpoint                      htsconstants.APIEndpoint
+	id, referenceName, start, end string
+	fields, tags, notags          []string
+	exp                           string
+}{
+	{
+		htsconstants.APIEndpointReadsTicket,
+		"object0052",
+		"chr1",
+		"65000",
+		"420000",
+		defaultListParameterValues["fields"],
+		defaultListParameterValues["tags"],
+		defaultListParameterValues["notags"],
+		"http://localhost:3000/reads/data/object0052?end=420000&referenceName=chr1&start=65000",
+	},
+	{
+		htsconstants.APIEndpointReadsTicket,
+		"tabulamuris.00001",
+		"chr22",
+		"11000000",
+		"45000000",
+		[]string{"SEQ", "QUAL"},
+		[]string{"NM", "HI"},
+		defaultListParameterValues["notags"],
+		"http://localhost:3000/reads/data/tabulamuris.00001?end=45000000&fields=SEQ%2CQUAL&referenceName=chr22&start=11000000&tags=NM%2CHI",
+	},
+}
+
 func TestConstructDataEndpointURL(t *testing.T) {
 
-	request := NewHtsgetRequest()
-	request.SetEndpoint(htsconstants.APIEndpointReadsTicket)
-	request.AddScalarParam("id", "object0069")
-	request.AddScalarParam("referenceName", "chr1")
-	request.AddScalarParam("start", "69000")
-	request.AddScalarParam("end", "4200000")
-	request.AddListParam("fields", defaultListParameterValues["fields"])
-	request.AddListParam("tags", defaultListParameterValues["tags"])
-	request.AddListParam("notags", defaultListParameterValues["notags"])
-	ep, _ := request.ConstructDataEndpointURL()
-	fmt.Println(ep)
-	assert.Equal(t, "a", "b")
+	for _, tc := range requestConstructDataEndpointURLTC {
+		request := NewHtsgetRequest()
+		request.SetEndpoint(tc.endpoint)
+		request.AddScalarParam("id", tc.id)
+		request.AddScalarParam("referenceName", tc.referenceName)
+		request.AddScalarParam("start", tc.start)
+		request.AddScalarParam("end", tc.end)
+		request.AddListParam("fields", tc.fields)
+		request.AddListParam("tags", tc.tags)
+		request.AddListParam("notags", tc.notags)
+		ep, _ := request.ConstructDataEndpointURL()
+		assert.Equal(t, tc.exp, ep.String())
+	}
 }
