29a3df35bc4fc00742efac752db286c229d561a4
Jeremy Adams <jeremy.adams@ga4gh.org>
2020-09-03 15:02:47 -0400

    update install samtools command

--- scripts/install-samtools.sh
+++ scripts/install-samtools.sh
@@ -1,7 +1,7 @@
 SAMTOOLS_VERSION="1.9"
 
-wget https://github.com/samtools/samtools/releases/download/${SAMTOOLS_VERSION}/samtools-${SAMTOOLS_VERSION}.tar.bz2 \
-tar xvjf samtools-${SAMTOOLS_VERSION}.tar.bz2
+wget https://github.com/samtools/samtools/releases/download/${SAMTOOLS_VERSION}/samtools-${SAMTOOLS_VERSION}.tar.bz2
+tar -xvjf samtools-${SAMTOOLS_VERSION}.tar.bz2
 cd samtools-${SAMTOOLS_VERSION}
 ./configure --prefix=/usr/local
 make
