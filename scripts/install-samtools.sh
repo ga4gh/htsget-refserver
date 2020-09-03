4701cf2b9c59a3e6b656201efa8494a45c5830fd
Jeremy Adams <jeremy.adams@ga4gh.org>
2020-09-03 15:22:56 -0400

    changedir after installation of samtools

--- scripts/install-samtools.sh
+++ scripts/install-samtools.sh
@@ -3,8 +3,8 @@ SAMTOOLS_VERSION="1.9"
 wget https://github.com/samtools/samtools/releases/download/${SAMTOOLS_VERSION}/samtools-${SAMTOOLS_VERSION}.tar.bz2
 tar -xjf samtools-${SAMTOOLS_VERSION}.tar.bz2
 cd samtools-${SAMTOOLS_VERSION}
-pwd
 ./configure --prefix=`pwd`
 make
 make install
 export PATH="`pwd`/bin:${PATH}"
+cd ..
