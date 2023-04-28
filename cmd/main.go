3155166524bc80cb7a6372380e478535284419c5
Joakim Bygdell <joakim.bygdell@nbis.se>
2023-04-28 10:59:46 +0200

    Use Infof instead of Printf

--- cmd/main.go
+++ cmd/main.go
@@ -53,7 +53,7 @@ func main() {
 
 func logRequest(handler http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("%s %s %s", r.RemoteAddr, r.Method, r.URL)
+		log.Infof("%s %s %s", r.RemoteAddr, r.Method, r.URL)
 		handler.ServeHTTP(w, r)
 	})
 }
